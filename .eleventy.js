8f108812a444582178d5ba66687569e92ce40cd3
umagg4 <toriworks0466@gmail.com>
2025-09-19 15:51:44 +0900

    10

--- .eleventy.js
+++ .eleventy.js
@@ -1,44 +1,40 @@
 module.exports = function(eleventyConfig) {
+const fs = require("fs");
 
- // 本番環境（production）の時だけ、パスの先頭にリポジトリ名を付与する
-  const isProduction = process.env.NODE_ENV === 'production';
+// 本番環境（production）の時だけ、パスの先頭にリポジトリ名を付与する
+const isProduction = process.env.NODE_ENV === "production";
 
-  // 画像をコピー
-  
-  eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy("src/css");
+// 画像をコピー
+eleventyConfig.addPassthroughCopy("src/images");
+eleventyConfig.addPassthroughCopy("src/css");
 
-  const fs = require("fs");
-
-  // おえかき用コレクション
+// おえかき用コレクション
 eleventyConfig.addCollection("galleryImages", function() {
-  const dir = "src/images/portfolio";
-  return fs.readdirSync(dir)
-    .filter(f => /\.(jpg|jpeg|png|gif)$/i.test(f))
-    .map(f => ({ 
-      fileName: f, 
-      url: "images/portfolio/" + f  // パスプレフィックスなし
-    }));
+ const dir = "src/images/portfolio";
+ return fs.readdirSync(dir)
+   .filter(f => /\.(jpg|jpeg|png|gif)$/i.test(f))
+   .map(f => ({
+     fileName: f,
+     url: (isProduction ? "/umanogomingo/images/portfolio/" : "/images/portfolio/") + f
+   }));
 });
 
-  // もでりんぐ用コレクション
-  eleventyConfig.addCollection("modelingImages", function() {
-    const dir = "src/images/3dcg";
-    return fs.readdirSync(dir)
-      .filter(f => /\.(jpg|jpeg|png|gif)$/i.test(f))
-      .map(f => ({ fileName: f, url: "/images/3dcg/"+ f }));
-  });
+// もでりんぐ用コレクション
+eleventyConfig.addCollection("modelingImages", function() {
+ const dir = "src/images/3dcg";
+ return fs.readdirSync(dir)
+   .filter(f => /\.(jpg|jpeg|png|gif)$/i.test(f))
+   .map(f => ({
+     fileName: f,
+     url: (isProduction ? "/umanogomingo/images/3dcg/" : "/images/3dcg/") + f
+   }));
+});
 
-  return {
-
-    // passthroughFileCopy: true, // 画像などをdistにコピーする設定など、他の設定はそのまま
-    pathPrefix : isProduction ?"/umanogomingo/" : "/",
-
-
-    dir: {
-      input: "src",
-      output: "docs",
-
-    }
-  };
+return {
+ pathPrefix: isProduction ? "/umanogomingo/" : "/",
+ dir: {
+   input: "src",
+   output: "docs",
+ }
+};
 };
